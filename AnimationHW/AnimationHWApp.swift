77c94a90b0ac8f1da899ad7c1b07d348f89727be
Luis Bustamante <luisebt995@gmail.com>
2024-04-10 15:11:33 +0200

    Initial Commit

---
+++ AnimationHW/AnimationHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationHWApp.swift
+//  AnimationHW
+//
+//  Created by Luis on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
